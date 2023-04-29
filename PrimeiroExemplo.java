4be36118cbc1acee89ddf08502e00dd4cc83b8b2
marcelo781 <39533676+marcelo781@users.noreply.github.com>
2023-04-29 16:18:28 -0300

    Ranqueando piratas usando Switch/Case
    Teste dos comandos de decisão Switch/Case

---
+++ PrimeiroExemplo.java
@@ -0,0 +1,46 @@
+package pratica1;
+
+import java.util.Scanner;
+
+public class PrimeiroExemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner(System.in);
+				
+		System.out.println("Diga seu nível de pirata pae");
+		
+		int pirata = entrada.nextInt();
+		
+		switch (pirata) {
+		
+		case 1: 
+			System.out.println("Pirata iniciante");
+			break;
+		case 2: 
+			System.out.println("Pirata prodígio");
+			break;
+		case 3: System.out.println("Pirata experiente");
+		break;
+		case 4: System.out.println("Pirata renomado");
+		break;
+		case 5: System.out.println("Pirata famoso");
+		break;
+		case 6: System.out.println("Schichibukai");
+		break;
+		case 7: System.out.println("Comandante de Yonkou");
+		break;
+		case 8: System.out.println("Imediato de Yonkou");
+		break;
+		case 9: System.out.println("Yonkou");
+		break;
+		case 10: System.out.println("Rei dos Piratas");
+		break;
+		}
+		
+		
+		entrada.close();
+	}
+
+}
